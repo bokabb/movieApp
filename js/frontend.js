31df10d9317cad30108e0815f49bda0ab4148e64
bokabb <espibgbg@gmail.com>
2018-02-22 13:46:33 +0100

    https://github.com/bokabb/movieApp.git

--- js/frontend.js
+++ js/frontend.js
@@ -1,44 +1,52 @@
 //prikaz funkcija samo za korisnike
 displayMoviesBlock();
 displayGenresBlock();
+tabItemsShow();
+
+var menuItems = document.getElementsByClassName("menuItem");
+for (var menu = 0; menu < menuItems.length; menu++) {
+    console.log("Dodajem klik na stavke u meniju!");
+    menuItems[menu].addEventListener("click", function (e) {
+        ajaxGetFileContent(e.target.getAttribute("data-page"));
+    })
+}
 
 function displayMoviesBlock() {
 
     var movies = getStorageData("movies");
 
     for (var movie in movies) {
-        document.getElementById("moviesBlock").innerHTML += "<div class='block-20 border tabItems " + movies[movie].genre + "'><a href='http://www.imdb.com/title/" + movies[movie].imdbNumber + "' target='_blank'><img src='/img/" + movies[movie].image + "' alt='" + movies[movie].title + "'><div class='block-100 border'>" + movies[movie].title + "</div></div>";
+        document.getElementById("moviesBlock").innerHTML += "<div class='block-20 tabItems " + movies[movie].genre + "'><a href='http://www.imdb.com/title/" + movies[movie].imdbNumber + "' target='_blank'><img src='/img/" + movies[movie].image + "' alt='" + movies[movie].title + "'><div class='block-100 black bold'>" + movies[movie].title + "</div></div>";
     }
 
 }
 
 function displayGenresBlock() {
-    
-        var genres = getStorageData("genres"),
-            counters=getCounters();
-    
-        document.getElementById("genresBlock").innerHTML += "<div class='block-20 border bold seablue filterTabItem' data-item='all'>Show All ("+counters.All +")</div>";
-        for (var genre in genres) {
-            var counter=counters[genres[genre].genreName] || 0;
-            document.getElementById("genresBlock").innerHTML += "<div class='block-20 border filterTabItem' data-item='" + genres[genre].genreName + "'>" + genres[genre].genreName + " ("+counter+")</div>";
-        }
-    }
-    function getCounters(){
-        var movies = getStorageData("movies"),
-            counter={},
-            counterAll=0;
-        for(var movie in movies){
-            if(typeof counter[movies[movie].genre]==='undefined'){
-                counter[movies[movie].genre]=0;
-            }
-            counter[movies[movie].genre]++;
-            counterAll++;
-        }
-        counter.All=counterAll;
-        return counter;
-    }
 
-tabItemsShow();
+    var genres = getStorageData("genres"),
+        counters = getCounters();
+
+    document.getElementById("genresBlock").innerHTML += "<div class='block-20 border bold seablue filterTabItem' data-item='all'>Show All (" + counters.All + ")</div>";
+    for (var genre in genres) {
+        var counter = counters[genres[genre].genreName] || 0;
+        document.getElementById("genresBlock").innerHTML += "<div class='block-20 border filterTabItem' data-item='" + genres[genre].genreName + "'>" + genres[genre].genreName + " (" + counter + ")</div>";
+    }
+}
+
+function getCounters() {
+    var movies = getStorageData("movies"),
+        counter = {},
+        counterAll = 0;
+    for (var movie in movies) {
+        if (typeof counter[movies[movie].genre] === 'undefined') {
+            counter[movies[movie].genre] = 0;
+        }
+        counter[movies[movie].genre]++;
+        counterAll++;
+    }
+    counter.All = counterAll;
+    return counter;
+}
 
 function tabItemsShow() {
 
@@ -47,16 +55,17 @@ function tabItemsShow() {
     for (var filter = 0; filter < tabs.length; filter++) {
         tabs[filter].addEventListener("click", function (e) {
             hideItems();
-            if (e.target.getAttribute("data-item") == "all") {
-                var itemsToShow = document.getElementsByClassName("tabItems");
-            } else {
-                var itemsToShow = document.getElementsByClassName(e.target.getAttribute("data-item"));
+
+            var className = e.target.getAttribute("data-item");
+            if (className === "all") {
+                className = "tabItems";
             }
+            var itemsToShow = document.getElementsByClassName(className);
 
             for (var item = 0; item < itemsToShow.length; item++) {
                 itemsToShow[item].style.display = "block";
             }
-        })
+        });
     }
 }
 
@@ -66,14 +75,6 @@ function hideItems() {
     for (var tab = 0; tab < items.length; tab++) {
         items[tab].style.display = "none";
     }
-}
-
-var menuItems = document.getElementsByClassName("menuItem");
-for (var menu = 0; menu < menuItems.length; menu++) {
-    console.log("Dodajem klik na stavke u meniju!");
-    menuItems[menu].addEventListener("click", function (e) {
-        ajaxGetFileContent(e.target.getAttribute("data-page"));
-    })
 }
 
 function ajaxGetFileContent(url) {
